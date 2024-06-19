65dac168df1e3eb4fae6daa50d99e42a3284eced
Gaurang Ingale <61673439+gaurang-ingale@users.noreply.github.com>
2024-06-19 14:05:45 +0200

    Add RPM Fusion repositories and the steam package

--- build.sh
+++ build.sh
@@ -14,7 +14,11 @@ RELEASE="$(rpm -E %fedora)"
 
 # this installs a package from fedora repos
 # rpm-ostree install screen
-# rpm-ostree install steam
+
+# Add RPM Fusion Non-Free repository
+rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \ 
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+rpm-ostree install steam
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
