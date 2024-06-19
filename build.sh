1e9c69b1f3b9e3ee44226437faba68714ed5615f
Gaurang Ingale <61673439+gaurang-ingale@users.noreply.github.com>
2024-06-19 13:43:34 +0200

    Steam package not found, removing it for now

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ RELEASE="$(rpm -E %fedora)"
 
 # this installs a package from fedora repos
 # rpm-ostree install screen
-rpm-ostree install steam
+# rpm-ostree install steam
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
