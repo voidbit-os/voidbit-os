15dffd62805051b5cdb5c890aa216d2f43ae4448
Gaurang Ingale <61673439+gaurang-ingale@users.noreply.github.com>
2024-06-19 14:11:57 +0200

    Try rebooting after adding repositories

--- build.sh
+++ build.sh
@@ -18,6 +18,9 @@ RELEASE="$(rpm -E %fedora)"
 # Add RPM Fusion Non-Free repository
 rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \ 
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+systemctl reboot
+
 rpm-ostree install steam
 
 # this would install a package from rpmfusion
