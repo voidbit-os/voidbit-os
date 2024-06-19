025194097da4c81e192a34b1d56901f8aa956881
Gaurang Ingale <61673439+gaurang-ingale@users.noreply.github.com>
2024-06-19 14:15:42 +0200

    Try separate commands

--- build.sh
+++ build.sh
@@ -16,10 +16,8 @@ RELEASE="$(rpm -E %fedora)"
 # rpm-ostree install screen
 
 # Add RPM Fusion Non-Free repository
-rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \ 
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-
-systemctl reboot
+rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+rpm-ostree install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 rpm-ostree install steam
 
